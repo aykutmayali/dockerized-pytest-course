f268fce6bc8325ad0bdb5b5bb6219c7ba6fb8c40
aykutmayali <aykutmayali@gmail.com>
2020-10-23 15:59:11 +0300

    isinstance

--- scripts/chp2/video2/mapmaker_start.py
+++ scripts/chp2/video2/mapmaker_start.py
@@ -1,6 +1,11 @@
 class Point():
     def __init__(self,name,latitude, longitude):
+        if not isinstance(name,str):
+            raise ValueError("City name provided must be a string")
         self._name = name
+
+        if not (-90<= latitude <=90) or not (-180 <= longitude <= 180):
+            raise ValueError("Invalid latitue,longitude combination")
         self._latitude = latitude
         self._longitude = longitude
 
--- tests/chp2/video2/test_mapmaker_start.py
+++ tests/chp2/video2/test_mapmaker_start.py
@@ -1,6 +1,16 @@
 from scripts.chp2.video2.mapmaker_start import Point
-
+import pytest
 
 def test_make_one_point():
     p1 = Point("Bagdad", 14.758, 18.898)
     assert p1.get_lang_long() == (14.758, 18.898)
+
+
+def test_invalid_point_generation():
+    with pytest.raises(ValueError) as exc:
+        Point("Senegal", 99.8548, -187.44406)
+    assert str(exc.value) == "Invalid latitue,longitude combination"
+
+    with pytest.raises(ValueError) as exc:
+        Point(5, 12.1152, -55.0805)
+    assert str(exc.value) == "City name provided must be a string"
