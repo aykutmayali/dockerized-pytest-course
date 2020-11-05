394b9eae8ab394e4182e183ee69999ab72815bc9
aykutmayali <aykutmayali@gmail.com>
2020-11-05 11:33:22 +0300

    sad-path-testing-1

--- tests/chp2/video7/test_sad_path_start.py
+++ tests/chp2/video7/test_sad_path_start.py
@@ -1,6 +1,6 @@
 import pytest
 
-# from scripts import data_processor
+from scripts import data_processor
 
 
 @pytest.fixture(scope="function")
@@ -16,4 +16,8 @@ def test_csv_reader_malformed_data_contents(city_list_location_malformed):
     in the exceptions context manager:
     """
     # data_processor.csv_reader(city_list_location_malformed)
-    pass
+    with pytest.raises(ValueError) as exp:
+        data_processor.csv_reader(city_list_location_malformed)
+    #pass
+    # assert str(exp.value) =="unsure"
+    assert str(exp.value) == "could not convert string to float: 'not_an_altitude'"
